1040d195a0df4d2686bdac7a2d4416b3a7b9ab77
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-02-09 21:55:42 +0600

    working

--- js/app.js
+++ js/app.js
@@ -1,30 +1,67 @@
-const searchSongs = () => {
+const searchMeals = () => {
     const searchText = document.getElementById('search-field').value;
-    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     // load data
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySongs(data.data))
+        .then(data => displayMeals(data.meals))
         .catch(error => displayError('Something Went Wrong!! Please try again later!'));
 }
-const displaySongs = songs => {
-    const songContainer = document.getElementById('song-container');
-    songContainer.innerHTML = '';
-    songs.forEach(song => {
-        const songDiv = document.createElement('div');
-        songDiv.className = 'single-result row align-items-center my-3 p-3';
-        songDiv.innerHTML = `
-        <div class="col-md-9">
-            <h3 class="lyrics-name">${song.title}</h3>
-            <p class="author lead">Album by <span>${song.artist.name}</span></p>
-            <audio controls>
-                <source src="${song.preview}" type="audio/mpeg">
-            </audio>
-        </div>
-        <div class="col-md-3 text-md-right text-center">
-            <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
-        </div>
-        `;
-        songContainer.appendChild(songDiv);
-    })
+
+
+// showing meal items from search result
+const displayMeals = meals => {
+    const mealsDiv = document.getElementById('meals');
+
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.className = 'meal';
+
+        const mealName = meal.strMeal;
+        const image = meal.strMealThumb;
+
+        const mealInfo = `
+                <div onclick='displayMealDetail("${mealName}")'>
+                <img class='meal-image' src="${image}">
+                <h3 class='meal-name'>${meal.strMeal}</h3>
+                </div>
+                `;
+
+        mealDiv.innerHTML = mealInfo;
+        mealsDiv.appendChild(mealDiv);
+
+    });
 }
+
+// fetching for specific meal detail info
+// const displayMealDetail = (name) => {
+
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => renderMealInfo(data.meals[0]));
+// }
+
+// // showing data for specific meal
+// const renderMealInfo = meal => {
+//     const mealDiv = document.getElementById("mealDetail");
+
+//     mealDiv.innerHTML = `
+//         <img src="${meal.strMealThumb}">
+//         <h3 class='meal-name'>${meal.strMeal}</h3>
+//         <h4>Ingredients</h4> 
+//         <li>${meal.strIngredient1}</li> 
+//         <li>${meal.strIngredient2}</li> 
+//         <li>${meal.strIngredient3}</li> 
+//         <li>${meal.strIngredient4}</li> 
+//         <li>${meal.strIngredient5}</li> 
+//         <li>${meal.strIngredient6}</li> 
+//         <li>${meal.strIngredient7}</li> 
+//         <li>${meal.strIngredient8}</li> 
+//         <li>${meal.strIngredient9}</li> 
+//         <li>${meal.strIngredient10}</li> 
+        
+//     `;
+
+// }
+// // setTimeout("location.reload(true)",20000);
